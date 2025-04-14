ed4cd3d95ad2db1040adb5e8339d9c440000d855
Rainux Luo <rainux@gmail.com>
2025-04-14 22:05:17 +0800

    Skip already downloaded files

--- download.go
+++ download.go
@@ -117,6 +117,21 @@ func downloadFile(urlStr string, filename string, tweet *twitterscraper.Tweet, c
 		}
 	}
 
+	// Skip if file already downloaded
+	if fileInfo, err := os.Stat(outputPath); err == nil {
+		resp, err := http.Head(urlStr)
+		if err != nil {
+			return eris.Wrap(err, "failed to check remote file size")
+		}
+
+		if resp.ContentLength > 0 {
+			if fileInfo.Size() == resp.ContentLength {
+				PrintInfoF("  Skipped: %s\n", outputPath)
+				return nil
+			}
+		}
+	}
+
 	// Send HTTP GET request
 	resp, err := http.Get(urlStr)
 	if err != nil {
